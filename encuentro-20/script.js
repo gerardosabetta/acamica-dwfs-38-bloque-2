061f04e4134de66551c825e492de355c7aa6cf71
Jerry <gerardo.sabetta@attackiq.com>
2020-07-21 22:15:36 -0300

    adds files created during class

---
+++ encuentro-20/script.js
@@ -0,0 +1,59 @@
+var nombre = "Gerardo";
+var edad = 24;
+var anioActual = "2020"; // ⚠️ Es un String!!
+var esProfe = true;
+
+// Para ver el tipo de las variables:
+console.log("El tipo de nombre es " + typeof nombre);
+console.log("El tipo de edad es " + typeof edad);
+console.log("El tipo de anioActual es " + typeof anioActual);
+console.log("El tipo de esProfe es " + typeof esProfe);
+
+// condicional
+if (esProfe == true) {
+  alert(nombre + ", es profesor");
+} else {
+  alert(nombre + ", no es profesor");
+}
+
+// Metodos de string:
+// String a mayusculas:
+console.log(nombre.toUpperCase());
+
+// String a minusculas
+console.log(nombre.toLowerCase());
+
+// Operadores aritmeticos
+
+console.log("2 + 2 = " + (2 + 2));
+console.log("2 - 2 = " + (2 - 2));
+console.log("2 * 2 = " + 2 * 2);
+console.log("2 / 2 = " + 2 / 2);
+
+// Operadores logicos
+
+console.log("false && false = " + false && false);
+console.log("false && true = " + false && true);
+console.log("true && true = " + true && true);
+console.log("false || false = " + false || false);
+console.log("false || true = " + false || true);
+console.log("true || true = " + true || true);
+console.log("!true = " + !true);
+console.log("!false = " + !false);
+
+// Operadores de comparacion
+
+// Igualdad
+console.log("2 es igual a 2?", 2 == 2);
+console.log("2 es igual a 3?", 2 == 3);
+
+// Mayor que
+
+console.log("4 es mayor a 2?", 4 > 2);
+console.log("2 es mayor a 4?", 2 > 4);
+
+// Menor que
+
+console.log("4 es menor a 2?", 4 < 2);
+console.log("2 es menor a 4?", 2 < 4);
+
