bfe2c9dd44185c6349684e8ef761ec4b53a54643
Jerry <gerardo.sabetta@attackiq.com>
2020-07-21 22:29:20 -0300

    ✨ adds files for inclass exercise

---
+++ encuentro-20/programemos.js
@@ -0,0 +1,7 @@
+var number1String = prompt("Ingrese el primer numero");
+var number2String = prompt("Ingrese el segundo numero");
+
+var number1 = parseInt(number1String);
+var number2 = parseInt(number2String);
+
+alert("La suma de sus numeros es " + (number1 + number2));
