e76265ff9d51664ee765bc3c372fe327ba83281a
Martin Soengas <martinsoengas@MacBook-Pro-de-Claudia.local>
2020-08-03 21:53:38 -0300

    nueva resolucion de encuentro 24

--- encuentro-24/ejercitacion_ms/script.js
+++ encuentro-24/ejercitacion_ms/script.js
@@ -1,62 +1,88 @@
-class Users {
+class User {
     constructor(name, lastname, email, username, password, active){
-    this.name;
-    this.lastname;
-    this.email;
-    this.username;
-    this.password;
-    this.active;
-    }
-    
-    addUser(name, lastname, email, username, password, active){
-        var name = prompt("Ingrese nombre");
-        var lastname = prompt("Ingrese apellido");
-        var email = prompt("Ingrese email");
-        var username = prompt("Ingrese nombre de usuario");
-        var password = prompt("Ingrese password");
-        var active = parseInt(prompt("ingrese estado 0 para inactivo, 1 para activo"));
-        users.push({"name": name, "lastname": lastname, "email": email, "username": username, "password": password ,"active": active});
+    this.name = name;
+    this.lastname = lastname;
+    this.email = email;
+    this.username = username;
+    this.password = password;
+    this.active = active;
     }
 };
+
+function userAdd(name,lastname,email,username,password,active){
+    users.push(new User(name,lastname,email,username,password,active));
+}
+
+// function userMod(name){
+
+// }
 
 const users = [
-    {
-        name: "Administrator",
-        lastname: "Admin",
-        email: "admin@admin.com",
-        username: "Admin",
-        password: "1234",
-        active: 1
-    }
+    new User("Administrador", "Admin", "admin@admin.com", "Admin", "1234", 1),
+    new User("Martin", "Soengas", "martin@soengas.com", "martin", "1234", 1)
 ];
 
-alert("Bienvenido a la creación/modificacion de usuarios, por favor ingrese un usuario administrador válido para acceder")
-let inputUsername = prompt("Ingresa tu usuario");
-let inputPassword = prompt("Ingresa tu password");
+console.log(users);
 
-const validarAdmin = (username,password) => {
-    try{
-        if (username === "" || password === "") {
-            throw new Error ("No ingresaste alguno o ninguno de los campos");
-        } 
-        else if (inputUsername === "Admin" && inputPassword === "1234") {
-            alert(`Bienvenido ${inputUsername} a la creación/modificación de usuarios`);
-            alert("A continuación podrá crear un nuevo usuario completando cada requerimiento del sistema, luego podra modificar usuarios ya existentes. Gracias")
-            do{
-                const userCreation = new Users();
-                userCreation.addUser();
-                var exitCreation = prompt("Si desea crear otro usuario, presione 'Aceptar', de lo contrario escriba 'Salir'");
-            }while(exitCreation !== "Salir");
-        } 
-        else {
-            throw new Error ("Usuario o contraseña no válido");
+// const userValidation = (username, password) => {
+//         if (username === "" || password === "") {
+//             alert("No ingresaste alguno de los campos");
+//         } else {
+//         for (let i = 0; i < users.length; i++) {
+//                 if (username === users[i].username && password === users[i].password && users[i].active === 1) {
+//                     alert(`Bienvenido ${inputUsername}`);
+//                     exit;
+//                 }else{
+//                     alert("Usuario o contraseña incorrectos");
+//                 }
+//             }
+//         }
+//   };
+
+// alert("A continuación ingrese su usuario y password para continuar")
+
+// let inputUsername = prompt("Ingresa tu usuario");
+// let inputPassword = prompt("Ingresa tu password");
+
+// userValidation(inputUsername, inputPassword);
+
+// alert("A continuación podrá crear nuevos usuarios")
+
+// do{
+//     var name = prompt("Ingrese nombre");
+//     var lastname = prompt("Ingrese apellido");
+//     var email = prompt("Ingrese email");
+//     var username = prompt("Ingrese nombre de usuario");
+//     var password = prompt("Ingrese password");
+//     var active = parseInt(prompt("ingrese estado 0 para inactivo, 1 para activo"));
+
+//     userAdd(name,lastname,email,username,password,active);
+
+//     var exit = prompt("Usuario creado con exito, si desea crear otro presione 'Aceptar', de lo contrario escriba 'Salir");
+
+// }while(exit !== "Salir")
+
+let searchResult = 0;
+
+function userMod(usermod){
+    for(i = 0; i < users.length; i++){
+        if(usermod == users[i].username){
+            users[i].name = prompt("Modifique el nombre");
+            users[i].lastname = prompt("Modifique el apellido");
+            users[i].email = prompt("Modifique el email");
+            users[i].username = prompt("Modifique el nombre de usuario");
+            users[i].password = prompt("Modifique el password");
+            users[i].active = parseInt(prompt("Modifique el estado"));
+            searchResult = 1;
         }
     }
-    catch (error) {
-        alert(error);
+    if(searchResult === 0){
+        alert("Usuario no encontrado");
     }
-};
+}
 
-validarAdmin(inputUsername,inputPassword);
+alert("A continuación podrá modificar usuarios buscandolos por su nombre de usuario")
 
-console.log(users)
+var inputUserMod = prompt("Ingrese nombre de usuario a modificar")
+
+userMod(inputUserMod);
