84a26e5522b4110a1fa796904ad70f3545e0d199
Martin Soengas <martinsoengas@MacBook-Pro-de-Claudia.local>
2020-08-03 21:55:49 -0300

    nueva resolucion de encuentro 24 (me habia olvidado de sacar los commets)

--- encuentro-24/ejercitacion_ms/script.js
+++ encuentro-24/ejercitacion_ms/script.js
@@ -24,43 +24,43 @@ const users = [
 
 console.log(users);
 
-// const userValidation = (username, password) => {
-//         if (username === "" || password === "") {
-//             alert("No ingresaste alguno de los campos");
-//         } else {
-//         for (let i = 0; i < users.length; i++) {
-//                 if (username === users[i].username && password === users[i].password && users[i].active === 1) {
-//                     alert(`Bienvenido ${inputUsername}`);
-//                     exit;
-//                 }else{
-//                     alert("Usuario o contraseña incorrectos");
-//                 }
-//             }
-//         }
-//   };
+const userValidation = (username, password) => {
+        if (username === "" || password === "") {
+            alert("No ingresaste alguno de los campos");
+        } else {
+        for (let i = 0; i < users.length; i++) {
+                if (username === users[i].username && password === users[i].password && users[i].active === 1) {
+                    alert(`Bienvenido ${inputUsername}`);
+                    exit;
+                }else{
+                    alert("Usuario o contraseña incorrectos");
+                }
+            }
+        }
+  };
 
-// alert("A continuación ingrese su usuario y password para continuar")
+alert("A continuación ingrese su usuario y password para continuar")
 
-// let inputUsername = prompt("Ingresa tu usuario");
-// let inputPassword = prompt("Ingresa tu password");
+let inputUsername = prompt("Ingresa tu usuario");
+let inputPassword = prompt("Ingresa tu password");
 
-// userValidation(inputUsername, inputPassword);
+userValidation(inputUsername, inputPassword);
 
-// alert("A continuación podrá crear nuevos usuarios")
+alert("A continuación podrá crear nuevos usuarios")
 
-// do{
-//     var name = prompt("Ingrese nombre");
-//     var lastname = prompt("Ingrese apellido");
-//     var email = prompt("Ingrese email");
-//     var username = prompt("Ingrese nombre de usuario");
-//     var password = prompt("Ingrese password");
-//     var active = parseInt(prompt("ingrese estado 0 para inactivo, 1 para activo"));
+do{
+    var name = prompt("Ingrese nombre");
+    var lastname = prompt("Ingrese apellido");
+    var email = prompt("Ingrese email");
+    var username = prompt("Ingrese nombre de usuario");
+    var password = prompt("Ingrese password");
+    var active = parseInt(prompt("ingrese estado 0 para inactivo, 1 para activo"));
 
-//     userAdd(name,lastname,email,username,password,active);
+    userAdd(name,lastname,email,username,password,active);
 
-//     var exit = prompt("Usuario creado con exito, si desea crear otro presione 'Aceptar', de lo contrario escriba 'Salir");
+    var exit = prompt("Usuario creado con exito, si desea crear otro presione 'Aceptar', de lo contrario escriba 'Salir");
 
-// }while(exit !== "Salir")
+}while(exit !== "Salir")
 
 let searchResult = 0;
 
