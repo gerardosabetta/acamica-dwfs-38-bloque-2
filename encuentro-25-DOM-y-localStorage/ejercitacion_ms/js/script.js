2e9f41b2d3bb0227195f2b87f4b120e4784558fe
Martin Soengas <martinsoengas@MacBook-Pro-de-Claudia.local>
2020-08-09 14:06:42 -0300

    se rehizo funcion de cambio de imagen utilizando setInterval

--- encuentro-25-DOM-y-localStorage/ejercitacion_ms/js/script.js
+++ encuentro-25-DOM-y-localStorage/ejercitacion_ms/js/script.js
@@ -1,22 +1,3 @@
-
-// function navbarBackground(){
-//     document.getElementById("navbar").classList.add('navbar-light');
-//     document.getElementById("navbar").classList.remove('bg-primary');
-
-//     if (document.getElementById("navbar").classList.contains('navbar-light'))
-//         document.getElementById("navbar").classList.toggle('navbar-light');
-
-//         // document.getElementById("navbar").classList.add('navbar-light');
-//         // document.getElementById("navbar").classList.remove('bg-primary');
-    
-//         // if (document.getElementById("navbar").classList.contains('bg-primary'))
-//         //     document.getElementById("navbar").classList.toggle('navbar-light');
-// }
-
-
-// console.log(y);
-
-
 // Agregar item
 function addItem(){
     let ul = document.querySelector('ul');
@@ -40,37 +21,60 @@ function addBold(){
 }
 
 
+
+/////////////////////////////////////
+
 // Cambio de imagen cada 2 segundos
 
-function changeImg()
-{
-    var img = document.getElementById("img1");
-    img.src = images[x];
-    x++;
+// function changeImg()
+// {
+//     var img = document.getElementById("img1");
+//     img.src = images[x];
+//     x++;
 
-    if(x >= images.length){
-        x = 0;
-    } 
+//     if(x >= images.length){
+//         x = 0;
+//     } 
 
-    setTimeout("changeImg()", 2000);
+//     setTimeout("changeImg()", 2000);
+// }
+
+// var images = [],
+// x = 0;
+
+// images[0] = "img/airplane-flying-right.png";
+// images[1] = "img/airplane-flying-left.png";
+// setTimeout("changeImg()", 2000);
+
+/////////////////////////////////////
+
+// nuevo cambio de imagen, solo una vez luego de 3 segundos
+
+var img1 = document.getElementById("img1");
+var imgAttribute = img1.getAttribute('src');
+
+setInterval(function(){ 
+    console.log(imgAttribute);
+    if(imgAttribute == "img/airplane-flying-right.png"){
+        changeImage("img/airplane-flying-left.png");
+    }else{
+        changeImage("img/airplane-flying-right.png"); 
+    }
+}, 3000);
+
+function changeImage(source) {
+    imgAttribute = source;
+    document.getElementById("img1").src = source;
 }
 
-var images = [],
-x = 0;
-
-images[0] = "img/airplane-flying-right.png";
-images[1] = "img/airplane-flying-left.png";
-setTimeout("changeImg()", 2000);
-
-
+// invertir imagen
 
 function invertImg() {
     var img = document.getElementById('img2').src;
 
     if (img.indexOf('img/many-airplanes-right.jpg')!=-1) {
         document.getElementById('img3').src = 'img/many-airplanes-right.jpg';
-        document.getElementById('img2').src  = 'img/many-airplanes-left.jpg';
-        
+        document.getElementById('img2').src  = 'img/many-airplanes-left.jpg';  
     }
      else {
        document.getElementById('img2').src = 'img/many-airplanes-right.jpg';
